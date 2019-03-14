d827b36b816bec1d948cb5a6a0c153899cb869a9
Reuben Vas <reuben.vas@appliedtechnology.se>
2019-03-14 11:59:15 +0100

    backend ready 4 action

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+const {promisify} = require('util');
+
 const fs = require('fs');
 
 const app = express();
@@ -18,8 +20,10 @@ app.post('/submit_userdata', (req, res) => {
     // should be sent in x-www-form-urlencoded
     // 'name=Reuben_Vas&car_number=ABC123&car_model=Volvo
     // try w/o underscores
+
     const accessKey = storeUserdataToFile(req.body);
-    res.send(200, accessKey);
+    // add som more status codes if something goes wrong
+    res.status(202).send(accessKey);
 })
 
 function storeUserdataToFile(userdata) {
@@ -27,12 +31,12 @@ function storeUserdataToFile(userdata) {
     const accessKey = generateUuid();
     const time = getTimeHours();
     const userInfo = {
-        ...userdata, 
+        ...userdata,
         time_of_arrival: time,
         access_key: accessKey
     };
 
-    fs.writeFile(`./server/userdata/${accessKey}.json`, JSON.stringify(userInfo), (err) => {
+    fs.writeFile(`./server/userdata/${accessKey}.txt`, JSON.stringify(userInfo), (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
     })
@@ -40,17 +44,32 @@ function storeUserdataToFile(userdata) {
 }
 
 function getTimeHours() {
-   const millisec = Date.now();
-   const hour = millisec / 3.6e6;
-   return Math.floor(hour);
+    const millisec = Date.now();
+    const hour = millisec / 3.6e6;
+    return Math.floor(hour);
 }
 
-app.get('/get_userdata', (req, res) => {
+app.get('/get_userdata', async (req, res) => {
     // use authorization
-    const accessKey = req.headers.authorization;
-    console.log(accessKey);
-    res.send('some data');
+    const userAccessKey = req.headers.authorization.split(' ')[1];
+    const userdataFiles = await getUserdataFiles();
+    const fileName = userdataFiles
+                        .map(fileName => fileName.split('.')[0])
+                        .find(accessKey => accessKey === userAccessKey);
+    if (fileName) {
+        const readfileProm = promisify(fs.readFile);
+        readfileProm(`./server/userdata/${fileName}.txt`, 'utf8')
+        .then(data => res.status(202).send(data))
+        .catch(err => console.error(err))
+    } else {
+        res.send(401)
+    }
 })
 
+function getUserdataFiles() {
+    const readdirProm = promisify(fs.readdir);
+    return readdirProm('./server/userdata')
+            .catch(err => console.error(err));
+}
 
 module.exports.app = app;
